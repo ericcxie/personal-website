1d37a7c295c0dd1d307924508d4d8d113d970c54
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2022-12-19 23:52:57 -0500

    Update header logo size

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       <main className='bg-white px-10'>
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className='text-xl font-poppins font-bold text-blue'>Eric Xie.</h1>
+            <h1 className='text-2xl font-poppins font-bold text-blue'>Eric Xie.</h1>
             <ul className='flex items-center'>
               <li>
                 <BsMoonFill className='cursor-pointer text-2xl'/>
