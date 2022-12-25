26d9eabcbcb142e4c591780578cfffc91f3e96fa
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2022-12-24 21:40:10 -0500

    Debug

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
               <div className='py-2 px-1 flex justify-start gap-4'>
                 <button className="font-poppins text-small bg-blue hover:bg-[#537399] text-white font-medium py-1.5 px-12 rounded-md pt-2 pb-2">Projects</button>
                 <button className='font-poppins font-medium text-blue hover:text-dark'>
-                  <Link href={'/resume'}>Resume</Link>
+                  <Link href={''}>Resume</Link>
                 </button>
               </div>
               <div className='text-2xl flex justify-start gap-1 pl-1 py-2 md:text-3xl dark:text-white md:hidden lg:hidden'>
--- pages/resume.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Resume() {
-    return (
-        <div>
-            <iframe src="https://drive.google.com/file/d/1OXMh5eZJjOtlf1Q0CsogtOGVadJUs3AY/view?usp=sharing"></iframe>
-        </div>
-    )
-  }
