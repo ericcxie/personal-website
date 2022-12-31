c29a787872533d2ea0bcd4d70dd5f4e523be95be
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2022-12-31 16:43:22 -0500

    Update experiences.js

--- components/sections/experiences.js
+++ components/sections/experiences.js
@@ -7,7 +7,7 @@ export default function Experiences() {
             <h1 className="text-3xl py-5 font-poppins font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-theme to-dark">
                 Experiences
             </h1>
-            <div className='mt-3 flex flex-col justify-center items-center'>
+            <div className='mt-3 flex flex-col justify-center items-start'>
             <ol className="relative border-l -pl-0 border-gray-200 dark:border-gray-700">                  
                 <li className="mb-10 ml-4">
                     <div className="absolute w-3 h-3 bg-theme rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
