bbe01551737fc0ab6b1d99c2740fd71113277fc5
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-01-01 16:35:59 -0500

    Update about.js

--- components/sections/about.js
+++ components/sections/about.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import portrait from "../../public/img/portrait.JPG";
+import portrait from "../../public/img/portrait.jpg";
 
 export default function About() {
     return (
