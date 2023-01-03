fa5f4d4c5d5173aa348c57a61d1403f1760f23fb
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-01-03 13:04:55 -0500

    Implement close menu outside element click
    Mobile menu will now close when the user clicks outside the menu element

--- components/navigation/navbar.js
+++ components/navigation/navbar.js
@@ -2,15 +2,32 @@ import { BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { AiFillLinkedin, AiFillGithub, AiOutlineInstagram, AiFillYoutube, AiOutlineTwitter, AiOutlineClose } from 'react-icons/ai';
 import { CgClose } from 'react-icons/cg';
 import { FiMenu } from 'react-icons/fi';
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { RxDividerVertical } from 'react-icons/rx';
 import { Transition } from "@headlessui/react";
 import { Link as ScrollLink } from 'react-scroll';
 import Link from 'next/link';
 
 
+
 export default function NavBar(props) {
     const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    useEffect(() => {
+      function handleClickOutside(event) {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+          setIsOpen(false);
+        }
+      }
+  
+    // Add event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+    // Unsubscribe from the event listener on cleanup
+      document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, [menuRef]);
 
     return (
       <nav className="bg-white py-2 z-20 px-7 pt-4 lg:py-5 md:px-20 lg:px-40 dark:bg-[#060813] fixed w-full left-0">
@@ -41,13 +58,13 @@ export default function NavBar(props) {
               </div>
               <li><RxDividerVertical className='dark:text-white text-lg lg:text-xl'/></li> 
               <li onClick={props.onClick} className='cursor-pointer dark:text-white'>
-                {props.darkMode ? <BsSunFill className='hover:text-[#FFE87C]'/> : <BsMoonFill className='hover:text-[#D8D6CB]'/> }
+                {props.darkMode ? <BsSunFill className='md:hover:text-[#FFE87C]'/> : <BsMoonFill className='md:hover:text-[#D8D6CB]'/> }
               </li>
             </ul>
             <ul className='flex lg:hidden flex-1 justify-end gap-4'>
               {/* <li><BsMoonFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl mt-1 dark:text-white'/></li> */}
               <li onClick={(props.onClick)} className='cursor-pointer text-2xl mt-1 dark:text-white'>
-                {props.darkMode ? <BsSunFill className='hover:text-[#FFE87C]'/> : <BsMoonFill className='hover:text-[#D8D6CB]'/> }
+                {props.darkMode ? <BsSunFill className='md:hover:text-[#FFE87C]'/> : <BsMoonFill className='md:hover:text-[#D8D6CB]'/> }
               </li>
               {/* mobile button */}
               <button> 
@@ -70,7 +87,7 @@ export default function NavBar(props) {
       >
         <div className="relative">
           <div className="hidden dark:inline absolute -inset-0.5 bg-gradient-to-r from-purple-700 via-blue-blue-500 to-blue-600 rounded-lg blur"></div>
-          <div onClick={() => setIsOpen(!isOpen)} className={isOpen ? 'relative lg:hidden ring-1 ring-zinc-900/5  font-poppins font-normal bg-white mt-3 py-2 text-theme dark:text-gray-200 shadow-lg shadow-zinc-800/5 rounded-2xl dark:bg-[#060813] text-center' : 'hidden'}>
+          <div ref={menuRef} onClick={() => setIsOpen(!isOpen)} className={isOpen ? 'relative lg:hidden ring-1 ring-zinc-900/5  font-poppins font-normal bg-white mt-3 py-2 text-theme dark:text-gray-200 shadow-lg shadow-zinc-800/5 rounded-2xl dark:bg-[#060813] text-center' : 'hidden'}>
           <ul>
             <li><ScrollLink onClick={() => setIsOpen(false)} to="home" spy={true} smooth={true} offset={-5} duration={700} className='block py-2 px-4 text-sm font-poppins cursor-pointer hover:text-dark dark:hover:text-gray-400'>Home</ScrollLink></li>
             <li><ScrollLink onClick={() => setIsOpen(false)} to="projects" spy={true} smooth={true} offset={-5} duration={700} className='block py-2 px-4 text-sm font-poppins cursor-pointer hover:text-dark dark:hover:text-gray-400'>Projects</ScrollLink></li>
