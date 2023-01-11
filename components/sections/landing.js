39a09352cf2b04610a40a497a3d1514cea7d61ac
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-01-11 16:13:49 -0500

    Added typing effect to landing page

--- components/sections/landing.js
+++ components/sections/landing.js
@@ -3,6 +3,7 @@ import { AiFillLinkedin, AiFillGithub, AiOutlineInstagram, AiFillYoutube, AiOutl
 import dots from '../../public/SVG/dots.svg';
 import Link from 'next/link';
 import { Link as ScrollLink } from 'react-scroll';
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Landing() {
 
@@ -10,16 +11,32 @@ export default function Landing() {
         <section id='home' className='flex mb-10 items-center md:justify-between py-24 md:px-90'>
             <div className='pb-28 md:pb-20'>
               <div className='pl-1 dark:text-white'> {/* Home page */}
-                <h2 className='py-2 font-poppins font-thin text-5xl md:text-6xl lg:text-7xl'>Hi,</h2>
+                <h2 className='py-2 ml-1 font-mono font-thin text-dark dark:text-gray-200 text-md lg:text-lg'>Hi, my name is</h2>
                 <div className='flex gap-2 justify-start text-5xl lg:text-6xl xl:text-7xl'>
-                  <h1 className='font-poppins font-bold tracking-tight'>I'm</h1>
-                  <h1 className='font-poppins font-bold underline underline-offset-3 decoration-theme decoration-7 tracking-tight md:decoration-8'>Eric Xie</h1>
+                  
+                  <h1 className='font-poppins font-bold underline underline-offset-3 decoration-theme decoration-7 tracking-tight '>Eric Xie.</h1>
                 </div>
-                <h3 className='font-poppins font-light py-2 text-2xl md:text-3xl'>Software Developer</h3>
+                <div className='ml-1 font-poppins mt-3 md:mt-4 font-light text-gray-800 dark:text-gray-100 py-2 text-xs md:text-md'>
+                <TypeAnimation
+                  // Same String at the start will only be typed once, initially
+                  sequence={[
+                  "I'm a Software Developer",
+                  1000,
+                  "I'm a Student",
+                  1000,
+                  "I'm a Tech Enthusiast",
+                  1000,
+                  ]}
+                  speed={35} // Custom Speed from 1-99 - Default Speed: 40
+                  style={{ fontSize: '2em' }}
+                  wrapper="span" // Animation will be rendered as a <span>
+                  repeat={Infinity} // Repeat this Animation Sequence infinitely
+                />
+              </div>
               </div>
               <div className='py-2 px-1 flex justify-start gap-4'>
                 <ScrollLink to="projects" spy={true} smooth={true} offset={-20} duration={700}>
-                  <div className="relative group">
+                  <div className="relative group mt-1">
                     <div className="hidden dark:inline absolute -inset-0.5 bg-gradient-to-r from-blue-500 to-purple-600 rounded-lg blur opacity-60 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
                     <button className="relative font-poppins text-small bg-theme hover:bg-dark dark:bg-[#0d122b] text-white dark:text-gray-100 font-medium py-1.5 px-12 rounded-lg pt-2 pb-2">
                       Projects 
@@ -41,6 +58,8 @@ export default function Landing() {
                 <a className='hover:text-[#00acee]' href="https://twitter.com/ericx_3"><AiOutlineTwitter/></a>
                 <a className='hover:text-[#6e5494]' href="https://github.com/ericcxie"><AiFillGithub/></a>
               </div>
+              
+              
             </div>
             <div className='hidden md:w-1/2 md:inline'><Image src={dots}/></div>
           </section>
