2db427b057f8c0d84b0a0903b6803f46756a265a
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-01-13 10:14:20 -0500

    Test apple.js page

--- pages/apple.js
+++ pages/apple.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Head from "next/head";
 import { useState, useEffect } from "react";
 
+
 export default function Apple() {
     const [darkMode, setDarkMode] = useState(false);
 
@@ -26,22 +27,18 @@ export default function Apple() {
                         <link rel="icon" href="/favicon.png" />
                 </Head>
                 
-                <div className=''>
+                <div className='bg-white dark:bg-darkmode h-screen w-screen'>
 
                 
                 <Link href='/'>
-                    <h1 className='px-4 py-2 text-2xl lg:text-2xl font-poppins font-bold text-theme cursor-pointer bg-clip-text dark:text-transparent dark:bg-gradient-to-r dark:from-[#3a7bd5] dark:to-[#2997c5]'>Eric Xie.</h1>
+                    <h1 className='px-4 py-2 text-2xl lg:text-2xl font-poppins font-bold text-theme cursor-pointer bg-clip-text dark:text-transparent dark:bg-gradient-to-r dark:from-[#3a7bd5] dark:to-[#2ada4d]'>Eric Xie.</h1>
                 </Link>
                 <div
                 class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
                 >
                 <div class="mx-auto max-w-3xl text-center">
                     <Image src={apple_logo} className='w-14 mx-auto py-4'/>
-                    <h1
-                    className="text-[#BDBDBD] text-3xl font-extrabold text-transparent sm:text-5xl"
-                    >
-                    My Apple Gear
-                    </h1>
+                    <h1 className="text-[#BDBDBD] dark:text-white text-3xl font-extrabold text-transparent sm:text-5xl"> My Apple Gear</h1>
                     <ul className='text-gray-500 text-center text-poppins mt-8'>
                         <li>MacBook Pro 14 inch 2022</li>
                         <li>iPhone 14 Pro</li>
