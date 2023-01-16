be437bc69db49624090e985263025cb251cbd284
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-01-15 20:44:24 -0500

    Testing contact form v2

--- components/sections/contactform.js
+++ components/sections/contactform.js
@@ -1,31 +1,74 @@
 import Aos from 'aos';
 import "aos/dist/aos.css"
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useFormik } from 'formik';
+import handleSubmit from '../utils/submitForm';
 
-export default function ContactForm() {
+export default function Contact() {
     useEffect(() => {
         Aos.init({ duration: 1000 });
       }, [])
+        
+        return (
+            <section>
+                <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+                    <div class="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
+                    <div class="lg:col-span-2 lg:py-12">
+                        <p class="max-w-xl text-3xl dark:text-white">
+                        Get In Touch
+                        </p>
 
-    return (
+                        <div class="mt-8">
+                        <p class="text-2xl font-bold dark:text-white">
+                            Have a question or want to have a quick ☕️ chat? Feel free to connect with me!
+                        </p>
+                        </div>
+                    </div>
 
-        <section>
-        <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-            <div class="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
-            <div class="lg:col-span-2 lg:py-12">
-                <p class="max-w-xl text-3xl dark:text-white">
-                Get In Touch
-                </p>
-
-                <div class="mt-8">
-                <p class="text-2xl font-bold dark:text-white">
-                    Have a question or want to have a quick ☕️ chat? Feel free to connect with me!
-                </p>
+                    <div class="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+                        <ContactForm/>
+                        
+                    </div>
+                    </div>
                 </div>
-            </div>
+        </section>
+          );
+        }
 
-            <div class="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
-                <form action="POST" data-netlify="true" class="space-y-4">
+
+        const validate = (values) => {
+            const errors = {};
+        
+            if (!values.name) {
+            errors.name = 'Please enter a name.';
+            }
+        
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+            errors.email = 'Please enter a valid email.';
+            }
+        
+            if (!values.message) {
+            errors.message = 'Please enter a message.';
+            }
+        
+            return errors;
+        };
+
+        function ContactForm() {
+            const [submitted, setSubmitted] = useState(false);
+        
+            const formik = useFormik({
+            initialValues: { name: '', email: '', message: '' },
+            validate,
+            onSubmit: (values, actions) => {
+                handleSubmit('contact', values);
+                actions.resetForm();
+                setSubmitted(true);
+            },
+            });
+
+        return (
+            <form name="contact" onSubmit={formik.handleSubmit} data-netlify="true" className="space-y-4">
                     <div>
                         <label class="sr-only" for="name">Name</label>
                         <input
@@ -33,7 +76,16 @@ export default function ContactForm() {
                         placeholder="Name"
                         type="text"
                         id="name"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        onFocus={() => setSubmitted(false)}
+                        value={formik.values.name}
                         />
+                        {formik.errors.name && formik.touched.name && (
+                            <p className="text-sm text-red-600 lg:text-base">
+                                {formik.errors.name}
+                            </p>
+                        )}
                     </div>
                     <div>
                         <label class="sr-only" for="email">Email</label>
@@ -42,7 +94,16 @@ export default function ContactForm() {
                         placeholder="Email address"
                         type="email"
                         id="email"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        onFocus={() => setSubmitted(false)}
+                        value={formik.values.email}
                         />
+                        {formik.errors.email && formik.touched.email && (
+                            <p className="text-sm text-red-600 lg:text-base">
+                            {formik.errors.email}
+                            </p>
+                        )}
                     </div>
 
                     <div>
@@ -52,37 +113,34 @@ export default function ContactForm() {
                         placeholder="Message"
                         rows="8"
                         id="message"
-                        ></textarea>
+                        type="text"
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                        onFocus={() => setSubmitted(false)}
+                        value={formik.values.message}
+                        />
+                        {formik.errors.message && formik.touched.message && (
+                            <p className="text-sm text-red-600 lg:text-base">
+                            {formik.errors.message}
+                            </p>
+                        )}
+                        {submitted && (
+                            <p className="text-sm text-theme dark:text-theme lg:text-base">
+                            Success! I will get back to you ASAP!
+                            </p>
+                        )}
                     </div>
 
                     <div class="mt-4">
                         <button
                         type="submit"
+                        purpose="submit"
+
                         class="inline-flex w-full items-center justify-center rounded-lg bg-black px-5 py-3 text-white sm:w-auto"
                         >
-                        <span class="font-medium"> Send Enquiry </span>
-
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="ml-3 h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                        >
-                            <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M14 5l7 7m0 0l-7 7m7-7H3"
-                            />
-                        </svg>
+                        <span class="font-medium"> Send message </span>
                         </button>
                     </div>
                 </form>
-            </div>
-            </div>
-        </div>
-        </section>
-
-    );
+        );
 }
