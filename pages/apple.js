1281548aac3674ac4795b698584c2a71bfcc282e
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-01-18 20:34:52 -0500

    Fixed warning message

--- pages/apple.js
+++ pages/apple.js
@@ -34,9 +34,9 @@ export default function Apple() {
                     <button className='px-4 py-2 text-2xl lg:text-2xl font-poppins font-bold text-theme cursor-pointer bg-clip-text dark:text-transparent dark:bg-gradient-to-r dark:from-[#3a7bd5] dark:to-[#2997c5]'>Eric Xie.</button>
                 </Link>
                 <div
-                class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
+                className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center"
                 >
-                <div class="mx-auto max-w-3xl text-center">
+                <div className="mx-auto max-w-3xl text-center">
                     <Image src={apple_logo} className='w-14 mx-auto py-4'/>
                     <h1 className="text-[#BDBDBD] dark:text-white text-3xl font-extrabold text-transparent md:text-5xl"> My Apple Gear</h1>
                     <ul className='text-gray-500 text-center text-poppins mt-8 dark:text-white'>
