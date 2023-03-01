5fd8038ae4d3e71885edc8ac240019bda1d9a4f2
Eric Xie <66566975+ericcxie@users.noreply.github.com>
2023-02-28 20:17:10 -0500

    Reduce landing load effect time

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1300);
   }, []);
 
   const toggleDarkMode = () => {
